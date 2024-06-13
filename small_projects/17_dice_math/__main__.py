530fcd256ad6bd9ee78ba2fdfad6e82866dd6c47
techrabbit58 <dzb@pruefbit.de>
2024-06-13 20:28:53 +0200

    #17 "dice math" solution ready, but does not distribute dice faces over the whole screen

--- small_projects/17_dice_math/__main__.py
+++ small_projects/17_dice_math/__main__.py
@@ -1,7 +1,13 @@
 import random
+import sys
+import time
+from textwrap import dedent
+
+import key_stroke
+from colterm import term
 
 from .dicefaces import die_face
-from .parameters import MIN_DICE, MAX_DICE
+from .parameters import MIN_DICE, MAX_DICE, QUIZ_DURATION, PENALTY, REWARD
 
 
 def roll(num_dice: int) -> tuple[int, list[list[str]]]:
@@ -14,11 +20,95 @@ def roll(num_dice: int) -> tuple[int, list[list[str]]]:
     return total, dice_faces
 
 
+def rules() -> str:
+    return dedent(f"""
+    The program rolls some dice and shows them to you.
+    You then have {QUIZ_DURATION} seconds to count the pips.
+    If you can enter the right number of pips while the time
+    counts down, you receive {REWARD} more points.
+    If you can not gie an answer before the time is over, your
+    score will decrease by {PENALTY} points.
+    """).strip()
+
+
+def sleep(seconds: float = 1.0) -> None:
+    try:
+        time.sleep(seconds)
+    except KeyboardInterrupt:
+        pass
+
+
+def wait_for_any_key(kb: key_stroke.Key_Stroke) -> None:
+    while not kb.kbhit():
+        sleep(.1)
+    term.get_key()
+
+
 def main() -> None:
-    num_dice = random.randint(MIN_DICE, MAX_DICE)
-    total, dice = roll(num_dice)
-    print('\n'.join('   '.join(row) for row in zip(*dice)))
-    print(f'These {num_dice} dice give a total of {total} pips.')
+    kb = key_stroke.Key_Stroke()
+
+    term.clear()
+    print(rules())
+
+    print('\nPress any key to continue.')
+    wait_for_any_key(kb)
+
+    correct_answers = incorrect_answers = score = 0
+
+    while True:
+        print('Guess the number of pips:')
+
+        num_dice = random.randint(MIN_DICE, MAX_DICE)
+        total, dice = roll(num_dice)
+
+        countdown = QUIZ_DURATION * 10
+        answer = ''
+        while countdown > 0:
+            term.clear()
+            term.hide_cursor()
+
+            term.fg('cyan')
+            print('\n'.join('   '.join(row) for row in zip(*dice)))
+
+            term.fg('yellow')
+            print(f'Your current score is {score} points.')
+            print(f'There are {num_dice} dice shown. Guess the total number of pips.')
+            print(f'You have {countdown // 10} seconds left.')
+
+            term.fg('reset')
+            term.show_cursor()
+
+            print(f'your guess> {answer}', end='', flush=True)
+
+            if kb.kbhit():
+                ch = term.get_key().lower()
+                if ch in '0123456789':
+                    answer += ch
+                elif (ch == 'q' and answer == '') or (ch == '\n' and len(answer) > 0):
+                    if answer == '':
+                        answer = 'quit'
+                    print()
+                    break
+                elif ch == '\b' and len(answer) > 0:
+                    answer = answer[:-1]
+            else:
+                sleep(.1)
+                countdown -= 1
+
+        if answer == 'quit':
+            break
+        else:
+            if countdown > 9 and int(answer) == total:
+                correct_answers += 1
+            else:
+                incorrect_answers += 1
+            score = correct_answers * REWARD - incorrect_answers * PENALTY
+
+    term.clear()
+    print('Correct answers:   ', correct_answers)
+    print('Incorrect answers: ', incorrect_answers)
+    print('Final score:       ', score)
+    print('Bye!')
 
 
 main()
--- small_projects/17_dice_math/parameters.py
+++ small_projects/17_dice_math/parameters.py
@@ -1,2 +1,5 @@
 MIN_DICE = 2
 MAX_DICE = 6
+QUIZ_DURATION = 30  # seconds
+PENALTY = 1
+REWARD = 4
