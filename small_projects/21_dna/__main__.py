70b5fea7aeb225db80157fc969d2ac5969b6c788
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2024-06-28 09:35:10 +0200

    #21 "dna visualization"

--- small_projects/21_dna/__main__.py
+++ small_projects/21_dna/__main__.py
@@ -1,5 +1,21 @@
+import random
+import sys
+import time
+
+from .helix import helix
+
+nucleotides = dict(zip('AGCT', 'TCGA'))
+
+
 def main() -> None:
-    print('Hello, world!')
+    for line in helix():
+        left = random.choice(list(nucleotides))
+        right = nucleotides[left]
+        print(line.format(left, right) if line.find('{') > -1 else line)
+        try:
+            time.sleep(1 / 16)
+        except KeyboardInterrupt:
+            sys.exit()
 
 
 main()
