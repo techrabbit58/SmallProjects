235a6a4983ff0cc86abf9cfaf851dc40768925f8
techrabbit58 <dzb@pruefbit.de>
2024-07-27 19:24:07 +0200

    #66 simple substitution cipher (to be refactored)

--- small_projects/66_simple_substitution_cipher/__main__.py
+++ small_projects/66_simple_substitution_cipher/__main__.py
@@ -1,17 +1,28 @@
+import random
 import shlex
-import textwrap
 from collections.abc import Callable
+
+import pyperclip
 
 
 class Shell:
     def __init__(self, prog: str) -> None:
         self._prog = prog
-        self._symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        self._key = None
+        self._symbols = 'abcdefghijklmnopqrstuvwxyz'
+        self._shuffled = self._keys = self._values = None
 
     def _get_action(self, candidate: str) -> Callable[[list[str]], bool | None]:
         candidate = f'do_{candidate.lower()}'
         return getattr(self, candidate, self.do_default)
+
+    def _show_key(self) -> None:
+        print(f'The key is "{"".join(self._shuffled).upper()}".')
+
+    def do_info(self, _: list[str]) -> None:
+        """INFO\t\tShow some runtime information."""
+        print(f'mode = {"ENCRYPT" if self._shuffled == self._values else "DECRYPT"}')
+        print(f' key = {None if self._shuffled is None else "".join(self._shuffled).upper()}')
+        print()
 
     def cmdloop(self) -> None:
         print('\n=====', self._prog, '=====\n')
@@ -31,19 +42,42 @@ class Shell:
             except (EOFError, IndexError):
                 pass
 
-    def do_random(self, args: list[str]) -> None:
+    def do_random(self, _: list[str]) -> None:
         """RANDOM\t\tGenerate a random key."""
+        self._shuffled = list(self._symbols)
+        random.shuffle(self._shuffled)
+        self._show_key()
+        self.do_encrypt()
 
     def do_key(self, args: list[str]) -> None:
         """KEY keyword(s)\tMake a new key from the given keyword(s)."""
-        raw_key = [ch.lower() for ch in ''.join(args[1:]) if ch in self._symbols]
-        print(f'"{"".join(raw_key)}"')
+        raw_key = [ch.lower() for ch in ''.join(args[1:]) if ch.lower() in self._symbols]
+        self._shuffled = []
+        remaining_symbols = list(self._symbols)
+        for ch in raw_key:
+            if ch in self._shuffled:
+                continue
+            self._shuffled.append(ch)
+            remaining_symbols.remove(ch)
+        self._shuffled += remaining_symbols
+        self._show_key()
+        self.do_encrypt()
 
-    def do_encrypt(self, args: list[str]) -> None:
+    def do_encrypt(self, _: list[str] = None) -> None:
         """ENCRYPT\t\tSwitch to encryption mode."""
+        if self._shuffled is None:
+            print('You must first create a key.')
+            return
+        self._keys, self._values = list(self._symbols), self._shuffled
+        print('Now in encryption mode.\n')
 
-    def do_decrypt(self, args: list[str]) -> None:
+    def do_decrypt(self, _: list[str] = None) -> None:
         """DECRYPT\t\tSwitch to decryption mode."""
+        if self._shuffled is None:
+            print('You must first create a key.')
+            return
+        self._keys, self._values = self._shuffled, list(self._symbols)
+        print('Now in decryption mode.\n')
 
     @staticmethod
     def do_quit(_: list[str]) -> bool:
@@ -58,10 +92,29 @@ class Shell:
                 print(getattr(self, attr).__doc__)
         print()
 
-    @staticmethod
-    def do_default(args: list[str]) -> None:
+    def do_default(self, args: list[str]) -> None:
         """line\t\tEncrypt or decrypt the given line of text. Depends on current mode."""
-        print(f'line of text: "{' '.join(args)}"')
+        if self._shuffled is None:
+            print('You must first create a key.')
+            return
+
+        out = []
+        for inp in ' '.join(args):
+            try:
+                offset = self._keys.index(inp.lower())
+            except ValueError:
+                offset = -1
+
+            if offset < 0:
+                out.append(inp)
+            elif inp.islower():
+                out.append(self._values[offset])
+            else:
+                out.append(self._values[offset].upper())
+
+        result = ''.join(out)
+        print(f'"{result}"\n')
+        pyperclip.copy(result)
 
 
 Shell('Simple Substitution Cipher').cmdloop()
