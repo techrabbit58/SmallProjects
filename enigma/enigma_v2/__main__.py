86ee7e03b341ab0ffe8efe787ba3416317854ba1
techrabbit58 <dzb@pruefbit.de>
2024-08-10 14:09:47 +0200

    enigma_v2: minor fix

--- enigma/enigma_v2/__main__.py
+++ enigma/enigma_v2/__main__.py
@@ -1,6 +1,6 @@
 from .enigma import EnigmaRocket
 from .procedures import casual_conversion
-from .symbols import number_string_to_symbols, symbols_to_number_string
+from .symbols import symbols_to_number_string
 
 # cryptext = 'DEUTS QETRU PPENS INDJE TZTIN ENGLA ND---'
 # cryptext = 'QSZVI DVMPN EXACM RWWXU IYOTY NGVVX DZ---'
