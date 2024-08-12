c1336af0918fd9812872b8217b0510ec4a092568
techrabbit58 <dzb@pruefbit.de>
2024-08-12 15:55:01 +0200

    enigma_v2: add more modern encoding procedure using Base64

--- enigma/enigma_v2/__main__.py
+++ enigma/enigma_v2/__main__.py
@@ -1,13 +1,17 @@
-from .enigma import EnigmaRocket
-from .procedures import casual_conversion
-from .symbols import symbols_to_number_string
+from .enigma import EnigmaM3
+from .procedures import base64_encrypt, base64_decrypt, in_groups
 
 # cryptext = 'DEUTS QETRU PPENS INDJE TZTIN ENGLA ND---'
 # cryptext = 'QSZVI DVMPN EXACM RWWXU IYOTY NGVVX DZ---'
 
-enigma = EnigmaRocket('III I II', symbols_to_number_string('ZQPM'))
-print('ring setting:', symbols_to_number_string('ZQPM'))
+enigma = EnigmaM3('C', 'VI V VIII', '17 16 13').set_jumpers('AB CD EF GH JK MN OP QR TU XZ')
 
-key = 'JEZA'
-casual_text = 'Ukrainische Truppen sind jetzt in der russischen Region Kursk.'
-print(casual_conversion(enigma, key, casual_text))
+key = 'QED'
+casual_text = 'Deutsche Truppen sind jetzt in England.'
+print(casual_text)
+
+ciphertext = in_groups(base64_encrypt(enigma, key, casual_text), 4)
+print(ciphertext)
+
+plaintext = base64_decrypt(enigma, key, ciphertext)
+print(plaintext)
--- enigma/enigma_v2/procedures.py
+++ enigma/enigma_v2/procedures.py
@@ -1,14 +1,51 @@
+import base64
+import string
+from itertools import pairwise
+
 from .enigma import Enigma
 
 
-def casual_conversion(enigma: Enigma, key: str, text: str) -> str:
+def base64_encrypt(enigma: Enigma, key: str, text: str) -> str:
     enigma.set_key(key)
 
     result = []
-    for symbol in text:
+    for symbol in base64.b64encode(text.encode()).decode():
         was_lowercase = symbol.islower()
         result.append(enigma.convert(symbol.upper()))
         if was_lowercase:
             result[-1] = result[-1].lower()
 
     return ''.join(result)
+
+
+def base64_decrypt(enigma: Enigma, key: str, text: str) -> str:
+    enigma.set_key(key)
+
+    result = []
+    for symbol in text:
+        if symbol in string.whitespace:
+            continue
+        was_lowercase = symbol.islower()
+        result.append(enigma.convert(symbol.upper()))
+        if was_lowercase:
+            result[-1] = result[-1].lower()
+
+    return base64.b64decode(''.join(result).encode()).decode()
+
+
+def in_groups(text: str, group_size: int = 5) -> str:
+    groups = []
+    single_group = []
+
+    for symbol in text:
+        if symbol == ' ':
+            continue
+
+        single_group.append(symbol)
+        if len(single_group) == group_size:
+            groups.append(''.join(single_group))
+            single_group = []
+
+    groups.append(''.join(single_group))
+
+    return ' '.join(groups)
