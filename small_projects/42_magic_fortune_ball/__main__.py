8909f278b61b789d26bf9deb24637e867961867d
techrabbit58 <dzb@pruefbit.de>
2024-09-19 11:51:17 +0200

    update: 42_magic_fortune_ball, more choices for replies and answers

--- small_projects/42_magic_fortune_ball/__main__.py
+++ small_projects/42_magic_fortune_ball/__main__.py
@@ -1,4 +1,5 @@
 import os.path
+import random
 import sys
 from pathlib import Path
 import configparser
@@ -6,8 +7,9 @@ import configparser
 
 def main() -> None:
     cfg = configparser.ConfigParser()
-    cfg.read(Path(os.path.dirname(sys.argv[0])) / 'magic_fortune_ball.conf')
-    print(cfg.get('DEFAULT', 'replies').strip().split('\n'))
+    cfg.read(Path(os.path.dirname(sys.argv[0])) / 'magic_fortune_ball.ini')
+    print(random.choice(cfg.get('DEFAULT', 'replies').strip().split('\n')))
+    print(random.choice(cfg.get('DEFAULT', 'answers').strip().split('\n')))
 
 
 main()
