3e9c385056955d8bcba1b697dee07df47dc03035
techrabbit58 <dzb@pruefbit.de>
2024-09-26 19:29:17 +0200

    update: #74 still w.i.p.

--- small_projects/74_text_to_speech/__main__.py
+++ small_projects/74_text_to_speech/__main__.py
@@ -1,5 +1,28 @@
+import argparse
+from typing import cast
+
+import pyttsx3, pyttsx3.voice
+
+
+def get_cli_args(prog: str) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog, description='Use the text-to-speech features of the pyttsx3 library to generate audio.')
+    parser.add_argument(
+        'text',
+        help='the text to convert to audible voice')
+    return parser.parse_args()
+
+
 def main(prog: str) -> None:
-    print(f'{prog}: Hello, world!')
+    args = get_cli_args(prog)
+    text = args.text
+    tts = pyttsx3.init()
+    voices = cast(list[pyttsx3.voice.Voice], tts.getProperty('voices'))
+    voice = voices[1]
+    tts.setProperty('voice', voice.id)
+    tts.setProperty('rate', 150)
+    tts.say(text)
+    tts.runAndWait()
 
 
 main('Text to Speech')
