3106de7c2a8c3ee918b90068bf442b2ea6ae3e32
techrabbit58 <dzb@pruefbit.de>
2024-09-29 12:15:13 +0200

    new: #78 "trick questions"

---
+++ small_projects/78_trick_questions/__main__.py
@@ -0,0 +1,29 @@
+import json
+import pathlib
+import random
+import sys
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class TrickQuestion:
+    question: str
+    answer: str
+    accept: list[str]
+
+
+def load_questionaire(name: str) -> list[TrickQuestion]:
+    with (pathlib.Path(sys.argv[0]).parent / name).open() as fp:
+        return [TrickQuestion(**item) for item in json.load(fp)]
+
+
+def main():
+    questions = load_questionaire('trick_questions.json')
+    index = random.randrange(len(questions))
+    item = questions[index]
+    print(item.question)
+    input('Press enter to see the right answer.')
+    print(item.answer)
+
+
+main()
