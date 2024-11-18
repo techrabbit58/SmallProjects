6ed3f078d0233dd46f0de3d242ba0bc16f061ed4
techrabbit58 <dzb@pruefbit.de>
2024-11-18 17:45:24 +0100

    update: #72 "sPOnGeCaSe"

--- small_projects/72_spongecase/__main__.py
+++ small_projects/72_spongecase/__main__.py
@@ -1,3 +1,4 @@
+import random
 import tkinter as tk
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
@@ -18,17 +19,25 @@ class App(tk.Tk):
 
         ttk.Label(f_left, text='Your Message', font=self.default_font).pack(padx=5, pady=5)
 
-        t_message = ScrolledText(f_left, width=20, height=5, font=self.text_font, spacing2=3, spacing3=3)
+        t_message = ScrolledText(
+            f_left, width=20, height=5, font=self.text_font, spacing2=3, spacing3=3, wrap='word')
         t_message.pack(padx=10, pady=5)
+
+        t_message.bind('<KeyRelease>', self.input_key_pressed)
+
+        self.message = t_message
 
         f_right = ttk.Frame(self)
         f_right.grid(row=1, column=1)
 
         ttk.Label(f_right, text='sPoNgECaSe', font=self.default_font).pack(padx=5, pady=5)
 
-        t_spongecase = ScrolledText(f_right, width=20, height=5, font=self.text_font, spacing2=3, spacing3=3)
+        t_spongecase = ScrolledText(
+            f_right, width=20, height=5, font=self.text_font, spacing2=3, spacing3=3, wrap='word')
         t_spongecase['state'] = 'disabled'
         t_spongecase.pack(padx=10, pady=5)
+
+        self.spongecase = t_spongecase
 
         f_buttons = ttk.Frame(self)
         f_buttons.grid(row=2, column=0, columnspan=2)
@@ -36,7 +45,7 @@ class App(tk.Tk):
         b_quit = ttk.Button(f_buttons, text='Quit', command=self.destroy)
         b_quit.pack(side=tk.LEFT, padx=5, pady=10, ipady=3)
 
-        b_clip = ttk.Button(f_buttons, text='sPoNgeCAsE to Clipboard')
+        b_clip = ttk.Button(f_buttons, text='sPoNgeCAsE to Clipboard', command=self.spongecase_to_clipboard)
         b_clip.pack(padx=5, pady=10, ipadx=10, ipady=3)
 
         f_bottom = ttk.Frame(self, height=5)
@@ -46,6 +55,28 @@ class App(tk.Tk):
         style.configure('TButton', font=self.default_font)
 
         t_message.focus()
+
+    def input_key_pressed(self, event: tk.Event) -> None:
+        if not event.keysym.isprintable():
+            return
+        to_upper = False
+        message = self.message.get(1.0, tk.END)
+        bucket = []
+        for ch in message:
+            bucket.append(ch.upper() if to_upper else ch.lower())
+            if random.random() > 0.1:
+                to_upper = not to_upper
+        spongecase = ''.join(bucket)
+        widget_state = self.spongecase['state']
+        self.spongecase['state'] = 'normal'
+        self.spongecase.delete(1.0, tk.END)
+        self.spongecase.insert(tk.END, spongecase.strip('\n'))
+        self.spongecase['state'] = widget_state
+
+    def spongecase_to_clipboard(self) -> None:
+        spongecase = self.spongecase.get(1.0, tk.END)
+        self.spongecase.clipboard_clear()
+        self.spongecase.clipboard_append(spongecase)
 
 
 App('sPoNGgEcAsE').mainloop()
