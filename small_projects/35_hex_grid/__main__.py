e3e176faf48de377e60706707686317b3fc0a00e
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2025-07-03 12:16:08 +0200

    update: #35 hex grid does now accept command line parameters for x_repeat and y_repeat

--- small_projects/35_hex_grid/__main__.py
+++ small_projects/35_hex_grid/__main__.py
@@ -1,14 +1,20 @@
-def main(x_repeat: int = 19, y_repeat: int = 12) -> None:
-
-    for y in range(y_repeat):
-
-        for x in range(x_repeat):  # Top half
-            print(r"/ \_", end="")
-        print()
-
-        for x in range(x_repeat):  # Bottom half
-            print(r"\_/ ", end="")
-        print()
+import argparse
 
 
-main(y_repeat=3)
+def main() -> None:
+
+    parser = argparse.ArgumentParser(prog="hexgrid")
+    parser.add_argument("--x_repeat",
+                        metavar="N", type=int, default=19,
+                        help="repeat the pattern horizontally N times")
+    parser.add_argument("--y_repeat",
+                        metavar="N", type=int, default=12,
+                        help="repeat the pattern vertically N times")
+    args = parser.parse_args()
+
+    for y in range(args.y_repeat):
+        print(r"/ \_" * args.x_repeat)  # Top half.
+        print(r"\_/ " * args.x_repeat)  # Bottom half.
+
+
+main()
