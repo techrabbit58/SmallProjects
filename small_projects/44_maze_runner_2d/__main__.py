327de728d944c11dd35f7a7e197a2a1d465e895a
techrabbit58 <dzb@pruefbit.de>
2025-07-07 17:34:40 +0200

    w.i.p.: #44 maze runner 2D

--- small_projects/44_maze_runner_2d/__main__.py
+++ small_projects/44_maze_runner_2d/__main__.py
@@ -1,4 +1,5 @@
 import textwrap
+from pathlib import Path
 
 
 def intro() -> None:
@@ -11,25 +12,74 @@ def intro() -> None:
 class Maze:
     def __init__(self) -> None:
         self.is_terminated_game = False
+
     def run(self) -> None:
-        print(f"Hello, {self.__class__.__name__}!")
         self.is_terminated_game = True
 
+    def parse(self, maze_text: list[str]) -> None:
+        print(maze_text)
 
-def setup_dialog() -> Maze:
+
+def setup_dialog() -> Maze | None:
+    while True:
+        print("Enter the filename of a .maze file (or LIST or QUIT).")
+        response = input("> ").strip()
+        if not response:
+            continue
+
+        if "QUIT".startswith(response.upper()):
+            print()
+            return None
+
+        if "LIST".startswith(response.upper()):
+            catalog = list(Path().glob("*.maze"))
+            if len(catalog) == 0:
+                print("No .maze files.")
+                print()
+                return None
+            print("All .maze files in the current directory:\n")
+            for file in catalog:
+                print(f"   {file.name}")
+            print()
+            continue
+
+        # At this point, the response shall be treated as a filename.
+        maze_file = Path(adjust_filename(response))
+        if not maze_file.exists():
+            print(f"The file '{maze_file.name}' does not exist. Try again.")
+            continue
+
+        # At this point we know, the .maze file exists and can now be loaded.
+        return make_maze(maze_file)
+
+
+def adjust_filename(response):
+    if not response.lower().endswith(".maze"):
+        response += ".maze"
+    return response
+
+
+def make_maze(maze_file) -> Maze:
     maze = Maze()
+    with maze_file.open() as fd:
+        maze.parse(fd.read().strip().split("\n"))
     return maze
+
+
+def finish() -> None:
+    print("Thank you for playing. Bye!\n")
 
 
 def main():
     while True:
         intro()
         maze = setup_dialog()
-        if maze.is_terminated_game:
+        if maze is None:
             break
         maze.run()
         if maze.is_terminated_game:
             break
+    finish()
 
 
 main()
