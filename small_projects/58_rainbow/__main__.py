05008471b14f4dafffdb96b0d04dc1cbd0b316bb
techrabbit58 <dzb@pruefbit.de>
2025-07-19 20:24:15 +0200

    final: 58_rainbow

--- small_projects/58_rainbow/__main__.py
+++ small_projects/58_rainbow/__main__.py
@@ -2,21 +2,23 @@ import time
 
 from colterm import term
 
-colors = "red yellow green blue cyan purple".split()
+COLOR_WIDTH = 4
+COLORS = "red yellow green blue cyan purple".split()
+RAINBOW_WIDTH = COLOR_WIDTH * len(COLORS)
 
 
 def show(indent: int) -> None:
     print(" " * indent, end="")
-    for color in colors:
+    for color in COLORS:
         term.fg(color)
-        print("##", end="")
+        print("#" * COLOR_WIDTH, end="")
     print()
     time.sleep(0.02)
 
 
 def advance(indent: int, is_indent_increasing: bool) -> tuple[int, bool]:
     indent += 1 if is_indent_increasing else -1
-    if indent == 0 or indent == term.size().columns - 13:
+    if indent == 0 or indent == term.size().columns - RAINBOW_WIDTH - 1:
         is_indent_increasing = not is_indent_increasing
     return indent, is_indent_increasing
 
