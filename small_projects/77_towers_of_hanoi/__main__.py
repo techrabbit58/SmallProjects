adda18eb95f6d392f96789b4da0ca600b57aa426
techrabbit58 <dzb@pruefbit.de>
2025-07-21 12:05:42 +0200

    new: 77_towers_of_hanoi (w.i.p.)

--- small_projects/77_towers_of_hanoi/__main__.py
+++ small_projects/77_towers_of_hanoi/__main__.py
@@ -4,9 +4,15 @@ after Al Sweigart's "Big Book of Small Python Projects"
     (https://inventwithpython.com/bigbookpython)
 """
 import textwrap
+from typing import TypeAlias
 
 TOTAL_DISKS = 5
 COMPLETE_TOWER = list(range(TOTAL_DISKS, 0, -1))
+
+
+OneTower: TypeAlias = list[int]
+TowerKey: TypeAlias = str
+TowerArrangement: TypeAlias = dict[TowerKey, OneTower]
 
 
 def ask_player(question: str, choices: list[str]) -> str:
@@ -30,10 +36,33 @@ def intro() -> None:
     """))
 
 
+def set_up_the_towers() -> TowerArrangement:
+    return {"A": COMPLETE_TOWER.copy(), "B": [], "C": []}
+
+
+def display_towers(towers: TowerArrangement) -> None:
+    for level in range(TOTAL_DISKS, -1, -1):
+        for key in "ABC":
+            tower = towers[key]
+            if level >= len(tower):
+                spaces = " " * TOTAL_DISKS
+                print(f"{spaces}||{spaces}", end="")
+            else:
+                disk = tower[level]
+                spaces = " " * (TOTAL_DISKS - disk)
+                solid = "@" * disk
+                print(f"{spaces}{solid}_{disk}{solid}{spaces}", end="")
+        print()
+
+
 def main() -> None:
     intro()
 
+    towers = set_up_the_towers()
+
     while True:
+        display_towers(towers)
+
         command = ask_player(
             "Enter the letters of the origin and destination towers, or (Q)uit.\n"
             "e.g. AB to move a disk from tower A to tower B.",
