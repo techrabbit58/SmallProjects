ad47899efdd8650c9918a74023e8ddd5edf4c41c
techrabbit58 <dzb@pruefbit.de>
2025-07-24 15:12:05 +0200

    done: 76_tic_tac_toe

--- small_projects/76_tic_tac_toe/__main__.py
+++ small_projects/76_tic_tac_toe/__main__.py
@@ -1,3 +1,4 @@
+import itertools
 from string import Template
 
 X, O, BLANK = "X", "O", " "
@@ -39,15 +40,71 @@ class Board:
             raise KeyError(self.bad_key.substitute(key=key))
         return self._board[index]
 
+    def is_full(self) -> bool:
+        return all(space != BLANK for space in self._board)
+
+    def has_won(self, player: str) -> bool:
+        return any((
+            (player == self["1"] == self["2"] == self["3"]),
+            (player == self["4"] == self["5"] == self["6"]),
+            (player == self["7"] == self["8"] == self["9"]),
+            (player == self["1"] == self["4"] == self["7"]),
+            (player == self["2"] == self["5"] == self["8"]),
+            (player == self["3"] == self["6"] == self["9"]),
+            (player == self["1"] == self["5"] == self["9"]),
+            (player == self["3"] == self["5"] == self["7"]),
+        ))
+
+
+def ask_player(question: str, choices: list[str]) -> str:
+    choice = ""
+    while not choice:
+        print(question)
+        answer = input("> ").strip()
+        if len(answer) == 0:
+            continue
+        choice = answer.upper()
+        if choice not in choices:
+            print(f"'{answer}' is not in {list(choices)}. Try again.")
+            choice = ""
+            continue
+    return choice
+
 
 def main() -> None:
     board = Board()
-    board["1"] = X
-    print(board)
-    if board["7"] == BLANK:
-        print("free")
-    else:
-        print("occupied")
+    lineup = itertools.cycle((X, O))
+
+    player = next(lineup)
+    while True:
+        print(board)
+        answer = ask_player(
+            f"What is player {player}'s move? (1-9 or Q)",
+            list(SPACES) + ["Q"]
+        )
+
+        if answer == "Q":
+            break
+
+        if board[answer] != BLANK:
+            print("This space is already occupied. Try another.")
+            continue
+
+        board[answer] = player
+
+        if board.has_won(player):
+            print(board)
+            print(f"Player {player} has won the game.")
+            break
+
+        if board.is_full():
+            print(board)
+            print("This is a tie.")
+            break
+
+        player = next(lineup)
+
+    print("Thanks for playing.")
 
 
 main()
