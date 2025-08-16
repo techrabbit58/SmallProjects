f039a797ede5f3528a0e7322cd3c1d4c7771c68e
techrabbit58 <dzb@pruefbit.de>
2025-08-16 14:16:43 +0200

    new: 71_sound_mimic (w.i.p., sound files)

--- small_projects/71_sound_mimic/__main__.py
+++ small_projects/71_sound_mimic/__main__.py
@@ -1,14 +1,40 @@
+import os
 from string import Template
 
+import httpx
 from playsound3 import playsound
 
 URL = Template("https://inventwithpython.com/sound${letter}.wav")
 
 
+class SoundFile:
+    def __init__(self, url: str) -> None:
+        self._url = url
+        self._name = os.path.dirname(__file__) + "\\" + url.split("/")[-1]
+        if not os.path.exists(self._name):
+            self._download()
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    def _download(self):
+        response = httpx.get(self.url)
+        with open(self.name, "wb") as f:
+            f.write(response.read())
+        print(f"Download complete: {self.url}")
+
+
 def main():
-    for letter in "FASADASAFASS":
-        print(letter)
-        playsound(URL.substitute(letter=letter))
+    files = {letter: SoundFile(URL.substitute(letter=letter)) for letter in "ASDF"}
+
+    for letter in "ASDFFFSSAD":
+        filename = files[letter].name
+        playsound(filename)
 
 
 main()
