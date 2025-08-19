a477d522eee3836813d5ed3997d32df663f2e7e9
techrabbit58 <dzb@pruefbit.de>
2025-08-19 18:25:42 +0200

    done: 71_sound_mimic

--- small_projects/71_sound_mimic/__main__.py
+++ small_projects/71_sound_mimic/__main__.py
@@ -1,10 +1,15 @@
 import os
+import random
+import shutil
+import textwrap
+import time
 from string import Template
 
 import httpx
 from playsound3 import playsound
 
 URL = Template("https://inventwithpython.com/sound${letter}.wav")
+ASDF = "ASDF"
 
 
 class SoundFile:
@@ -29,12 +34,70 @@ class SoundFile:
         print(f"Download complete: {self.url}")
 
 
-def main():
-    files = {letter: SoundFile(URL.substitute(letter=letter)) for letter in "ASDF"}
+def intro() -> str:
+    return textwrap.dedent("""
+    * * *   S o u n d   M i m i c   * * *
+    
+    Try to memoize a pattern of A S D and F letters (each with its own sound)
+    as it gets longer and longer.
+    """)
 
-    for letter in "ASDFFFSSAD":
-        filename = files[letter].name
-        playsound(filename)
+
+def main():
+    print(intro())
+    input("Press Enter to begin ...")
+
+    files = get_sound_files()
+    game_loop(files)
+
+    print("Thank you for playing!\n")
+
+
+def get_sound_files():
+    return {letter: SoundFile(URL.substitute(letter=letter)) for letter in "ASDF"}
+
+
+def game_loop(files):
+    pattern = ""
+
+    while True:
+        clear_screen()
+
+        pattern += next_letter()
+
+        print("Pattern: ", end="")
+        for letter in pattern:
+            print(letter, end="", flush=True)
+            filename = files[letter].name
+            playsound(filename)
+
+        time.sleep(1)
+        clear_screen()
+
+        answer = input("Enter the pattern:\n> ").strip().upper()
+
+        if answer != pattern:
+            print(f"Incorrect!\nThe pattern was: {pattern}")
+            for letter in pattern:
+                filename = files[letter].name
+                playsound(filename)
+
+            print(f"You scored {len(pattern) - 1} points.")
+            break
+
+        print("Correct!")
+        time.sleep(1)
+
+
+def clear_screen():
+    _, height = shutil.get_terminal_size((80, 24))
+    for _ in range(height + 1):
+        print()
+
+
+def next_letter():
+    pattern = random.choice(ASDF)
+    return pattern
 
 
 main()
