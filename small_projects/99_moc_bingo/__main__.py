0187d591a0314651ca0d6166364053dd50e384fc
techrabbit58 <dzb@pruefbit.de>
2025-09-15 15:07:36 +0200

    99_moc_bingo still not done

--- small_projects/99_moc_bingo/__main__.py
+++ small_projects/99_moc_bingo/__main__.py
@@ -22,23 +22,27 @@ def distribute(row: list[int]) -> list[int | None]:
     return distributed_row
 
 
-def pick_a_row(decades: list[list[int]], candidates: list[int]) -> list[int | None]:
-    candidate_decades = sorted(candidates[:5])
-    row = []
+def pick_a_row(decades: list[list[int]]) -> list[int | None]:
+    candidate_decades = list(range(len(decades)))
+    candidate_decades.sort(key=lambda x: len(decades[x]), reverse=True)
+    candidate_decades = [candidate for candidate in candidate_decades if len(decades[candidate])]
+    random.shuffle(candidate_decades)
+    row, n = [], 5
     for index in candidate_decades:
         row.append(decades[index].pop())
+        n -= 1
+        if n == 0:
+            break
     return distribute(row)
 
 
 def pick_all_rows(decades: list[list[int]]) -> list[list[int | None]]:
     rows = []
     candidates = list(range(9))
-    for _ in range(17):
+    for _ in range(18):
         candidates = [(n + 1) % 9 for n in candidates]
-        row = pick_a_row(decades, candidates)
+        row = pick_a_row(decades)
         rows.append(row)
-    row = pick_a_row(decades, [n for n, decade in enumerate(decades) if decade])
-    rows.append(row)
     random.shuffle(rows)
     return rows
 
@@ -48,7 +52,11 @@ def main() -> None:
     all_rows = pick_all_rows(decades)
     for group in range(6):
         for group_row in range(3):
-            print(group + 1, group_row + 1, all_rows[group * 3 + group_row])
+            row = all_rows[group * 3 + group_row]
+            print(group + 1, group_row + 1, "--", end=" ")
+            for num in row:
+                print(f"[{num:2}] " if num else "[  ] ", end="")
+            print()
         print()
 
 
