275f9bffdd9ce1ac42a065cbf809f86f00a31521
techrabbit58 <dzb@pruefbit.de>
2025-09-18 19:30:22 +0200

    refactored: 68_sliding_tile_puzzle (done)

--- small_projects/68_sliding_tile_puzzle/__main__.py
+++ small_projects/68_sliding_tile_puzzle/__main__.py
@@ -30,88 +30,88 @@ def intro():
     """)
 
 
-def find_blank_space(board: list[list[str]]) -> tuple[int, int]:
-    for x in range(4):
-        for y in range(4):
-            if board[x][y] == BLANK:
-                return x, y
-    return -1, -1  # this line shall never be reached
+class Board:
+    def __init__(self, num_moves: int = 200):
+        self.board = deepcopy(NEW_BOARD)
+        self.blank = self._find_blank_space()
+        for _ in range(num_moves):
+            self._make_random_move()
 
-
-def apply_move(board: list[list[str]], *, move: str) -> None:
-    bx, by = find_blank_space(board)
-    if move == "W":
-        board[bx][by], board[bx][by + 1] = board[bx][by + 1], board[bx][by]
-    if move == "A":
-        board[bx][by], board[bx + 1][by] = board[bx + 1][by], board[bx][by]
-    if move == "S":
-        board[bx][by], board[bx][by - 1] = board[bx][by - 1], board[bx][by]
-    if move == "D":
-        board[bx][by], board[bx - 1][by] = board[bx - 1][by], board[bx][by]
-
-
-def make_random_move(board: list[list[str]]) -> None:
-    blankx, blanky = find_blank_space(board)
-    valid_moves = []
-    if blanky != 3:
-        valid_moves.append("W")
-    if blankx != 3:
-        valid_moves.append("A")
-    if blanky != 0:
-        valid_moves.append("S")
-    if blankx != 0:
-        valid_moves.append("D")
-    apply_move(board, move=random.choice(valid_moves))
-
-
-def create_new_puzzle(num_moves: int = 200) -> list[list[str]]:
-    puzzle = deepcopy(NEW_BOARD)
-    for i in range(num_moves):
-        make_random_move(puzzle)
-    return puzzle
-
-
-def render_the_board(board: list[list[str]]) -> str:
-    labels = []
-    for y in range(4):
+    def _find_blank_space(self) -> tuple[int, int]:
+        board = self.board
         for x in range(4):
-            labels.append(board[x][y].ljust(2))
-    return textwrap.dedent("""
-    +------+------+------+------+
-    |      |      |      |      |
-    |  {}  |  {}  |  {}  |  {}  |
-    |      |      |      |      |
-    +------+------+------+------+
-    |      |      |      |      |
-    |  {}  |  {}  |  {}  |  {}  |
-    |      |      |      |      |
-    +------+------+------+------+
-    |      |      |      |      |
-    |  {}  |  {}  |  {}  |  {}  |
-    |      |      |      |      |
-    +------+------+------+------+
-    |      |      |      |      |
-    |  {}  |  {}  |  {}  |  {}  |
-    |      |      |      |      |
-    +------+------+------+------+
-    """.format(*labels))
+            for y in range(4):
+                if board[x][y] == BLANK:
+                    return x, y
+        return -1, -1  # this line shall never be reached
 
+    def apply(self, move: str) -> None:
+        board = self.board
+        blankx, blanky = self.blank
+        if move == "W":
+            board[blankx][blanky], board[blankx][blanky + 1] = board[blankx][blanky + 1], board[blankx][blanky]
+        if move == "A":
+            board[blankx][blanky], board[blankx + 1][blanky] = board[blankx + 1][blanky], board[blankx][blanky]
+        if move == "S":
+            board[blankx][blanky], board[blankx][blanky - 1] = board[blankx][blanky - 1], board[blankx][blanky]
+        if move == "D":
+            board[blankx][blanky], board[blankx - 1][blanky] = board[blankx - 1][blanky], board[blankx][blanky]
+        self.blank = self._find_blank_space()
 
-def get_next_move(board: list[list[str]]) -> str:
-    blankx, blanky = find_blank_space(board)
-    w = "W" if blanky != 3 else " "
-    a = "A" if blankx != 3 else " "
-    s = "S" if blanky != 0 else " "
-    d = "D" if blankx != 0 else " "
-    answer = None
-    while not answer:
-        print(f"                       ({w})")
-        print(f"Enter WASD (or Q): ({a}) ({s}) ({d})")
-        answer = input("> ").strip().upper()
-        if answer in "QUIT" + (w + a + s + d).replace(" ", ""):
-            continue
+    def _make_random_move(self) -> None:
+        blankx, blanky = self.blank
+        valid_moves = []
+        if blanky != 3:
+            valid_moves.append("W")
+        if blankx != 3:
+            valid_moves.append("A")
+        if blanky != 0:
+            valid_moves.append("S")
+        if blankx != 0:
+            valid_moves.append("D")
+        self.apply(move=random.choice(valid_moves))
+
+    def render(self) -> str:
+        board = self.board
+        labels = []
+        for y in range(4):
+            for x in range(4):
+                labels.append(board[x][y].ljust(2))
+        return textwrap.dedent("""
+        +------+------+------+------+
+        |      |      |      |      |
+        |  {}  |  {}  |  {}  |  {}  |
+        |      |      |      |      |
+        +------+------+------+------+
+        |      |      |      |      |
+        |  {}  |  {}  |  {}  |  {}  |
+        |      |      |      |      |
+        +------+------+------+------+
+        |      |      |      |      |
+        |  {}  |  {}  |  {}  |  {}  |
+        |      |      |      |      |
+        +------+------+------+------+
+        |      |      |      |      |
+        |  {}  |  {}  |  {}  |  {}  |
+        |      |      |      |      |
+        +------+------+------+------+
+        """.format(*labels))
+
+    def get_next_move(self) -> str:
+        blankx, blanky = self.blank
+        w = "W" if blanky != 3 else " "
+        a = "A" if blankx != 3 else " "
+        s = "S" if blanky != 0 else " "
+        d = "D" if blankx != 0 else " "
         answer = None
-    return answer
+        while not answer:
+            print(f"                       ({w})")
+            print(f"Enter WASD (or Q): ({a}) ({s}) ({d})")
+            answer = input("> ").strip().upper()
+            if answer in "QUIT" + (w + a + s + d).replace(" ", ""):
+                continue
+            answer = None
+        return answer
 
 
 def main() -> None:
@@ -119,21 +119,21 @@ def main() -> None:
     print(intro())
     input("Press Enter to begin...")
 
-    board = create_new_puzzle()
+    board = Board()
 
     while True:
         clear_screen()
-        print(render_the_board(board))
-        move = get_next_move(board)
+        print(board.render())
+        move = board.get_next_move()
 
         if move in {"Q", "QUIT"}:
             break
 
-        apply_move(board, move=move)
+        board.apply(move)
 
         if board == NEW_BOARD:
             clear_screen()
-            print(render_the_board(board))
+            print(board.render())
             print("You got it!")
             break
 
