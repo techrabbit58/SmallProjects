11ec6f7657857819fb7dfacd08785d7fce06e17b
techrabbit58 <dzb@pruefbit.de>
2025-09-19 11:25:58 +0200

    refactored: 68_sliding_tile_puzzle (tried to make code cleaner)

--- small_projects/68_sliding_tile_puzzle/__main__.py
+++ small_projects/68_sliding_tile_puzzle/__main__.py
@@ -71,13 +71,15 @@ class Board:
             valid_moves.append("D")
         self.apply(move=random.choice(valid_moves))
 
-    def render(self) -> str:
+    def __str__(self) -> str:
         board = self.board
         labels = []
         for y in range(4):
             for x in range(4):
                 labels.append(board[x][y].ljust(2))
         return textwrap.dedent("""
+        Sliding Tile Puzzle
+        
         +------+------+------+------+
         |      |      |      |      |
         |  {}  |  {}  |  {}  |  {}  |
@@ -97,21 +99,31 @@ class Board:
         +------+------+------+------+
         """.format(*labels))
 
-    def get_next_move(self) -> str:
+    @property
+    def valid_moves(self) -> tuple[str, str, str, str]:
         blankx, blanky = self.blank
         w = "W" if blanky != 3 else " "
         a = "A" if blankx != 3 else " "
         s = "S" if blanky != 0 else " "
         d = "D" if blankx != 0 else " "
+        return w, a, s, d
+
+    @property
+    def is_ordered(self) -> bool:
+        return self.board == NEW_BOARD
+
+
+def get_next_move(valid_moves: tuple[str, str, str, str]) -> str:
+    w, a, s, d = valid_moves
+    answer = None
+    while not answer:
+        print(f"                          ({w})")
+        print(f"Enter WASD (or Quit): ({a}) ({s}) ({d})")
+        answer = input("> ").strip().upper()
+        if answer in f"QUIT Q {w} {a} {s} {d}".split():
+            continue
         answer = None
-        while not answer:
-            print(f"                       ({w})")
-            print(f"Enter WASD (or Q): ({a}) ({s}) ({d})")
-            answer = input("> ").strip().upper()
-            if answer in "QUIT" + (w + a + s + d).replace(" ", ""):
-                continue
-            answer = None
-        return answer
+    return answer
 
 
 def main() -> None:
@@ -123,17 +135,17 @@ def main() -> None:
 
     while True:
         clear_screen()
-        print(board.render())
-        move = board.get_next_move()
+        print(board)
+        move = get_next_move(board.valid_moves)
 
         if move in {"Q", "QUIT"}:
             break
 
         board.apply(move)
 
-        if board == NEW_BOARD:
+        if board.is_ordered:
             clear_screen()
-            print(board.render())
+            print(board)
             print("You got it!")
             break
 
